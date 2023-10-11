ac25d53e800895f2679cc71f014856d939837336
jmh <hyeok123456789@gmail.com>
2023-10-11 18:38:24 +0900

    갤러리 파일 보완

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,27 @@
-//파일불러오는건 노드js로 해야하는듯하다.
+// //파일불러오는건 노드js로 해야하는듯하다.
+
+// var gallery = {
+//     gallery:()=>{
+//         const fs = require('fs');
+
+//         fs.readdir('gallery', (err, files)=>{
+//             console.log(files);
+
+//             for(var i=0;i<files.length;i++){
+//                 fs.readFile()
+//             }
+//         })
+//     }
+// };
+
+// module.exports = gallery;
+
+var galleryContainer = document.querySelector('.gallery-container');
+
+var imageList = 13;
+
+for(var i=1;i<=imageList;i++){
+    galleryContainer.innerHTML += `
+        <img src = './gallery/${i}.jpeg' class='gallery-image'>
+    `;
+}
---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+
+var app = http.createServer((request, response)=>{
+    var _url = request.url;
+    var fileName = url.parse(_url, false).pathname;
+
+    // if(fileName === '/Gallery.html'){
+    //     let galleryHTML = fs.readFileSync(__dirname + fileName,'utf8');
+    //     let newDoc = Document.createElement('div');
+    //     newDoc.innerHTML = galleryHTML;
+    //     console.log(newDoc.querySelect('.gallery-container'));
+
+    //     response.end(fs.readFileSync(__dirname + fileName));
+    // }else{
+    //     response.end(fs.readFileSync(__dirname + fileName));
+    // }
+
+    response.end(fs.readFileSync(__dirname + fileName));
+});
+
+app.listen(3000);
